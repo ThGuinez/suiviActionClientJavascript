f6ed938a5015d0b7f1b82285f49120dd8bddd543
guinez.cdi03 <guinez.cdi03@LAT-08.angers.imie>
2016-03-09 17:31:58 +0100

    Ajouter client

--- src/main/webapp/js/documentReady.js
+++ src/main/webapp/js/documentReady.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+	// TODO: si nul créer un tableau vide
 	listeDesClients = JSON.parse(localStorage.getItem('listeClients'));
 	listeActionsClients = JSON.parse(localStorage.getItem('listeActions'));
 });
