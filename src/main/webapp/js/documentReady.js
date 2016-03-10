70c9ed9c6392115c100abb0febe705328a5598f7
guinez.cdi03 <guinez.cdi03@LAT-08.angers.imie>
2016-03-10 09:35:22 +0100

    MAJ document.ready

--- src/main/webapp/js/documentReady.js
+++ src/main/webapp/js/documentReady.js
@@ -10,10 +10,18 @@ function recuiperationLocalStorage(){
 	else{
 		listeDesClients = JSON.parse(localStorage.getItem('listeClients'));
 	}
+	
 	if(JSON.parse(localStorage.getItem('listeActions'))==undefined ){
 		listeActionsClients = new Array();
 	}
 	else{
-		listeActionsClients = JSON.parse(localStorage.getItem('listeActions'));
+		listeActionsClients = JSON.parse(localStorage.getItem('hisroriqueEtats'));
+	}
+	
+	if(JSON.parse(localStorage.getItem('hisroriqueEtats'))==undefined ){
+		listeHistoriqueEtat = new Array();
+	}
+	else{
+		listeHistoriqueEtat = JSON.parse(localStorage.getItem('hisroriqueEtats'));
 	}
 }
