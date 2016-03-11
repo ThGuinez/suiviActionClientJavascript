0449320c9969919ff9cbfbfa06bb629c71719c36
guinez.cdi03 <guinez.cdi03@LAT-08.angers.imie>
2016-03-11 14:25:01 +0100

    MAJ

--- src/main/webapp/js/changementEtat.js
+++ src/main/webapp/js/changementEtat.js
@@ -4,8 +4,8 @@
 		$(".listeEtatTableau").change(function() {
 			actionident = $(this).parent("td").parent("tr").attr('actionid');
 			var val = $(".listeEtatTableau").val();
-			//alert(actionident);
-			//alert(val);
+			alert(actionident);
+			alert(val);
 			// on enregistre un objet historique
 			var date = new Date();
 			var stringDate=date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
@@ -27,66 +27,6 @@
 	}
 
 $(function() {
-
-
-	
 	detectClicChang();
-
-	function supprimerAction(id) {
-
-		var i = 0;
-		var dataDelete = false;
-
-		if (JSON.parse(localStorage.getItem('listeActions')) == undefined) {
-			listeActionsClients = new Array();
-		} else {
-			listeActionsClients = JSON.parse(localStorage
-					.getItem('listeActions'));
-		}
-
-		
-		while (dataDelete == false && i < listeActionsClients.length) {
-			//alert("A");
-			if (listeActionsClients[i].id == id) {
-				//alert("B:"+listeActionsClients[i].id);
-				var listAc = new ListeActions(listeActionsClients);
-				listeActionsClients = listAc.supprimer(
-						listAc.tableauActions, listeActionsClients[i]);
-				localStorage.listeActions= JSON.stringify(listeActionsClients);
-				dataDelete = true;
-				generationTableauActions();	
-				detectClicSupp();
-			}
-			i = i + 1;
-		}
-	}
-
-	
-	$("#validerSuppressionAction").click(function(){
-		//alert(actionid);
-		supprimerAction(actionid);
-	});
-	
 });
 
-// function supprimerAction(tableauActions,action){
-// var index = tableauActions.indexOf(action);
-// tableauActions.splice(index,1);
-//	
-// }
-//
-// function Action(id,titre,etat,client){
-// this.ref = ref;
-// this.date = date;
-// this.etat = etat;
-// }
-//
-//
-// function ListeHistoriqueEtat(historique){
-// this.tableauHistorique = historique;
-// this.ajouter = ajouterHistorique;
-// }
-//
-// function ajouterHistorique(tableauHistorique,changementEtat){
-// tableauHistorique.push(changementEtat);
-// }
--- src/main/webapp/js/documentReady.js
+++ src/main/webapp/js/documentReady.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	generationListeDeroulanteEtat(listeDesClients);
 	generationTableauActions()
 	detectClicHisto();
-
+	detectClicChang
 	
 });
 
