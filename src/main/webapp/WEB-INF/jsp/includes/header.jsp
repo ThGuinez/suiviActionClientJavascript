1a65844200757e02b8b6b185d9b8991746c5b686
guinez.cdi03 <guinez.cdi03@LAT-08.angers.imie>
2016-03-14 15:52:31 +0100

    MAJ

---
+++ src/main/webapp/WEB-INF/jsp/includes/header.jsp
@@ -0,0 +1,67 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Suivi d'actions Clients</title>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <c:url value="/outils/bootstrap/js/bootstrap.min.js" var="bootstrapJsUrl"/>
+  <script src="${bootstrapJsUrl}"></script>
+   
+<!-- scripts js -->
+
+<c:url value='js/documentReady.js' var="documentReady"/>
+<script src="${documentReady}"></script>
+
+<c:url value='js/action.js' var="actionjs"/>
+<script src="${actionjs}"></script>
+
+<c:url value='js/client.js' var="clientjs"/>
+<script src="${clientjs}"></script>
+
+<c:url value='js/historique.js' var="historiquejs"/>
+<script src="${historiquejs}"></script>
+
+<c:url value='js/supprimerAction.js' var="supprimerAction"/>
+<script src="${supprimerAction}"></script>
+
+<c:url value='js/ajouterClient.js' var="ajouterClient"/>
+<script src="${ajouterClient}"></script>
+
+<c:url value='js/ajouterAction.js' var="ajouterAction"/>
+<script src="${ajouterAction}"></script>
+
+<c:url value='js/historiqueEtat.js' var="historiqueEtat"/>
+<script src="${historiqueEtat}"></script>
+
+<c:url value='js/changementEtat.js' var="changementEtat"/>
+<script src="${changementEtat}"></script>
+
+
+
+
+<!-- Bootstrap -->
+<c:url value='/outils/bootstrap/css/bootstrap.min.css' var="bootstrapCssUrl"/>
+<link href="${bootstrapCssUrl}" rel="stylesheet">
+<c:url value='/css/base.css' var="baseCssUrl"/>
+<link href="${baseCssUrl}" rel="stylesheet">
+
+
+
+
+
+
+<!--  jQuery UI -->
+<!-- <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/cupertino/jquery-ui.css"> -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script> -->
+
+<!-- GOOGLE FONT ---->
+<!-- <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> -->
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
---
+++ src/main/webapp/js/documentReady.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+
+	tableauEtat = new Array();
+	tableauEtat[0] = "Début projet";
+	tableauEtat[1] = "En cours";
+	tableauEtat[2] = "Devis envoyé";
+	tableauEtat[3] = "Devis accepté et signé";
+	tableauEtat[4] = "Réalisé";
+	tableauEtat[5] = "Fini";
+	tableauEtat[6] = "Fin projet";
+	tableauEtat[7] = "Litige";
+	tableauEtat[8] = "Abandonné";
+	
+	recuperationLocalStorage();
+	generationListeDeroulanteClientFormAction(listeDesClients);
+	generationListeDeroulanteEtat(listeDesClients);
+	generationTableauActions()
+	detectClicHisto();
+	detectClicChang
+	
+});
+
+function recuperationLocalStorage() {
+	
+	if(localStorage.getItem("listeClients") == null){
+		localStorage.setItem("listeClients","[]");
+	}
+	if(localStorage.getItem("listeActions") == null){
+	localStorage.setItem("listeActions","[]");
+	}
+	if(localStorage.getItem("historiqueEtats") == null){
+	localStorage.setItem("historiqueEtats","[]");
+	}
+	
+	// clients
+		listeDesClients = JSON.parse(localStorage.getItem('listeClients'));
+	// actions
+		listeActionsClients = JSON.parse(localStorage.getItem('listeActions'));
+	// historique
+		listeHistoriqueEtat = JSON.parse(localStorage
+				.getItem('historiqueEtats'));
+}
+
+function generationListeDeroulanteClientFormAction(listeDesClients) {
+	var codeHTML = "";
+	for (var i = 0; i < listeDesClients.length; i++) {
+		codeHTML += '<option value="' + listeDesClients[i].id + '">'
+				+ listeDesClients[i].nom + '</option>';
+	}
+	// alert(codeHTML);
+	$("#listeDeroulanteClients").html(codeHTML);
+}
+
+function generationListeDeroulanteEtat() {
+	var codeHTML = "";
+	for (var i = 0; i < tableauEtat.length; i++) {
+		codeHTML += '<option value="' + i + '">' + tableauEtat[i] + '</option>';
+	}
+	// alert(codeHTML);
+	$("#listeDeroulanteEtat").html(codeHTML);
+}
+
+function generationListeDeroulanteEtatAvecSelected(etat) {
+	var codeHTMLB = "";
+	var stringSelected ="";
+	codeHTMLB += '<select class="listeEtatTableau">';
+
+	for (var z = 0; z < tableauEtat.length; z++) {
+		if(z==etat){
+			stringSelected = " selected ";
+		}
+		codeHTMLB += '<option value="' + z +'"'+stringSelected+'>' + tableauEtat[z] + '</option>';
+		stringSelected ="";
+	}
+	codeHTMLB += '</select>';
+	// alert(codeHTML);
+	return codeHTMLB;
+}
+
+
+function generationTableauActions() {
+
+	recuperationLocalStorage();
+	var tableHTML = "";
+	var listeCli = new ListeClients(listeDesClients);
+	if (listeActionsClients != null) {
+		for (var i = 0; i < listeActionsClients.length; i++) {
+
+			tableHTML += '<tr actionid='+listeActionsClients[i].id+'><td>'
+					+ listeActionsClients[i].titre
+					+ '</td><td>'
+					+ listeCli.getNom(listeCli.tableauClients,
+							listeActionsClients[i].client) + '</td><td>'
+					// + tableauEtat[listeActionsClients[i].etat]
+					+ generationListeDeroulanteEtatAvecSelected(listeActionsClients[i].etat)
+					+ '</td><td><button type="button" class="btn btn-default btn-lg boutHistorique" data-toggle="modal" data-target="#myModalHistoriqueEtat"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span></button></td>'
+					+'<td><button type="button" class="btn btn-default btn-lg boutSuppression" data-toggle="modal" data-target="#myModalSuppressionAction"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button></td></tr>';
+		}
+		$("#contenuTableau").html(tableHTML);
+	}
+}
